a901a433b85a4e5d8cd11bbe5c855de4bf14e0a9
SAFNAS <iamsafnas@gmail.com>
2020-05-16 21:19:25 +0530

    added articles app and related template and urls

--- djangonautic/urls.py
+++ djangonautic/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url
+from django.conf.urls import url,include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^articles/', include('articles.urls')),
     url(r'^about/$',views.about),
     url(r'^$',views.homepage)
     #path('about/',views.about),
