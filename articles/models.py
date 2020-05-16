86654538d5e919c273360d9925b13fbdb9e4c481
SAFNAS <iamsafnas@gmail.com>
2020-05-16 22:02:37 +0530

    added model, then showed in admin

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    # add a thumbnail later
+    # add a author later
+
+    def __str__(self):
+        return self.title
+
+
+# python manage.py makemigrations
+# python manage.py migrate
