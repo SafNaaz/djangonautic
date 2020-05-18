9c1e8823090ad262966581afd5cd74910f946c7f
SAFNAS <iamsafnas@gmail.com>
2020-05-18 18:05:37 +0530

    logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ app_name = 'accounts'
 
 urlpatterns = [
      url(r'^signup/$',views.signup_view, name="signup"),
-     url(r'^login/$',views.login_view, name="login")
+     url(r'^login/$',views.login_view, name="login"),
+     url(r'^logout/$',views.logout_view, name="logout")
 ]
